b26c8cffe8a26049e2c9a209b3abc2812668f313
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2018-02-10 15:34:59 -0600

    KEK test

--- VPythonTest/vp_test.py
+++ VPythonTest/vp_test.py
@@ -1,27 +1,40 @@
 from vpython import *
 
+
+def spawnCubeAt(x, y, z, l, w, h, c):
+    box1 = box(pos=vector(x, y, z), length=l, width=w, height=h, color=c)
+    return box1
+
+
+KEK = spawnCubeAt(0, 1, 0, 1, 1, 1, color.green)
+
+
 def keyInput(evt):
     s = evt.key
     if evt.key == "up":
-    	scene.camera.pos = vector(scene.camera.pos.x, scene.camera.pos.y + 0.5, scene.camera.pos.z)
+        KEK.pos = KEK.pos + KEK.axis
     elif evt.key == "down":
-    	scene.camera.pos = vector(scene.camera.pos.x, scene.camera.pos.y - 0.5, scene.camera.pos.z)
+        KEK.pos = KEK.pos - KEK.axis
     elif evt.key == "left":
-    	scene.camera.pos = vector(scene.camera.pos.x - 0.5, scene.camera.pos.y, scene.camera.pos.z)
+        KEK.rotate(radians(90), vec(0, 1, 0))
     elif evt.key == "right":
-    	scene.camera.pos = vector(scene.camera.pos.x + 0.5, scene.camera.pos.y, scene.camera.pos.z)
+        KEK.rotate(radians(-90), vec(0, 1, 0))
 
-def spawnCubeAt(x, y, z, l, w, h, c):
-	box1 = box(pos = vector(x, y, z), length = l, width = w, height = h, color = c)
-	return box1
 
-def init_floor( x , y ):
-    for i in range(0,x):
-        for j in range(0,y):
+def init_floor(x, y):
+    for i in range(0, x):
+        for j in range(0, y):
             spawnCubeAt(i, 0, j, 1, 1, 1, color.green)
 
+
+def rotate_obj(obj, axis, degrees):
+    pass
+
+
 def main():
-	scene.bind('keydown', keyInput)
-	init_floor(10, 10)
+    scene.bind('keydown', keyInput)
+    scene_floor = spawnCubeAt(0, 0, 0, 10, 10, 1, color.green)
+
+    attach_trail(KEK)
 
 main()
