5a97da870ac63f1420869f5d3b25717c5658e0ab
Gerardo Galvez <galvezgerardo01@gmail.com>
2018-02-10 14:13:34 -0600

    Add camera movement, doesn't work as expected

--- VPythonTest/vp_test.py
+++ VPythonTest/vp_test.py
@@ -1,9 +1,27 @@
 from vpython import *
 
+def keyInput(evt):
+    s = evt.key
+    if evt.key == "up":
+    	scene.camera.pos = vector(scene.camera.pos.x, scene.camera.pos.y + 0.5, scene.camera.pos.z)
+    elif evt.key == "down":
+    	scene.camera.pos = vector(scene.camera.pos.x, scene.camera.pos.y - 0.5, scene.camera.pos.z)
+    elif evt.key == "left":
+    	scene.camera.pos = vector(scene.camera.pos.x - 0.5, scene.camera.pos.y, scene.camera.pos.z)
+    elif evt.key == "right":
+    	scene.camera.pos = vector(scene.camera.pos.x + 0.5, scene.camera.pos.y, scene.camera.pos.z)
+
+def spawnCubeAt(x, y, z, l, w, h, c):
+	box1 = box(pos = vector(x, y, z), length = l, width = w, height = h, color = c)
+	return box1
+
 def init_floor( x , y ):
     for i in range(0,x):
         for j in range(0,y):
-            box(pos=vec(i, 0, j), radius=1, color=color.green)
+            spawnCubeAt(i, 0, j, 1, 1, 1, color.green)
 
 def main():
-    init_floor(10, 10)
+	scene.bind('keydown', keyInput)
+	init_floor(10, 10)
+
+main()
