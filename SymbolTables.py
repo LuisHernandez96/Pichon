e4e7e36e93a8a2d87360534f655a8aaff014a00a
Gerardo Galvez <galvezgerardo01@gmail.com>
2018-03-10 17:46:33 -0600

    Symbol Table

--- SymbolTables.py
+++ SymbolTables.py
@@ -4,6 +4,10 @@ ENV = "env"
 MOV = "mov"
 RETURN_TYPE = "returnType"
 VARS = "vars"
+DATA_TYPE = "data_type"
+VALUE = "value"
+SIZE = "size"
+LIST = "list"
 
 SYMBOL_TABLE = dict()
 
@@ -19,3 +23,17 @@ def ADD_FUNC(id, returnType, vars):
         SYMBOL_TABLE[FUNC][id][VARS] = vars
     else:
         print("ERROR, S_T[FUNC] DOESNT EXIST")
+
+def VARS_INIT():
+	VARS_TABLE = dict()
+	return VARS_TABLE
+
+def ADD_VAR(vars_table, id, data_type, value = None, size = None, isList = False):
+	if id in vars_table:
+		print("ERROR: " + id + " ALREADY DEFINED")
+	else:
+		vars_table[id] = dict()
+		vars_table[DATA_TYPE] = data_type
+		vars_table[VALUE] = value
+		vars_table[LIST] = isList;
+		vars_table[SIZE] = size
