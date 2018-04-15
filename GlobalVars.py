eb994a51a6ef5f306d4b01294e7365cead7079ae
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2018-04-15 12:26:56 -0500

    saved dims

--- GlobalVars.py
+++ GlobalVars.py
@@ -1,37 +1,40 @@
 class GlobalVars:
 
-	def nextTmp(self):
-		nextTemporal = "tmp" + str(self.tmpCounter)
-		self.tmpCounter = self.tmpCounter + 1
-		return nextTemporal
+    def nextTmp(self):
+        nextTemporal = "tmp" + str(self.tmpCounter)
+        self.tmpCounter = self.tmpCounter + 1
+        return nextTemporal
 
-	def prevTmp(self):
-		prevTemporal = "tmp" + str(self.tmpCounter - 1)
-		return prevTemporal
+    def prevTmp(self):
+        prevTemporal = "tmp" + str(self.tmpCounter - 1)
+        return prevTemporal
 
-	def __init__(self):
-		self.currentVarsTable = None
-		self.currentDataType = -1
-		self.currentId = ''
-		self.currentSize = 1
-		self.currentScope = "FUNCTIONS"
-		self.cuadruplos = []
-		self.operadores = []
-		self.saltos = []
-		self.tipos = []
-		self.operandos = []
-		self.tmpCounter = 1
-		self.cuadCounter = 0
-		self.lineNumber = 0
-		self.currentDataTypeString = ""
-		self.parameterCounter = 0
-		self.functionCalled = ""
-		self.functionReturns = False
-		self.lastDataType = -1
-		self.arrayPendingAddress = []
-		self.arrayPendingTypes = []
-		self.isAssigning = False
-		self.isInitializing = False
-		self.assigningID = ""
-		self.readList = False
+    def __init__(self):
+        self.currentVarsTable = None
+        self.currentDataType = -1
+        self.currentId = ''
+        self.currentSize = 1
+        self.currentScope = "FUNCTIONS"
+        self.cuadruplos = []
+        self.operadores = []
+        self.saltos = []
+        self.tipos = []
+        self.operandos = []
+        self.tmpCounter = 1
+        self.cuadCounter = 0
+        self.lineNumber = 0
+        self.currentDataTypeString = ""
+        self.parameterCounter = 0
+        self.functionCalled = ""
+        self.functionReturns = False
+        self.lastDataType = -1
+        self.arrayPendingAddress = []
+        self.arrayPendingTypes = []
+        self.isAssigning = False
+        self.isInitializing = False
+        self.assigningID = ""
+        self.readList = False
+        self.saved_dims = []
+
+
 globals = GlobalVars()
