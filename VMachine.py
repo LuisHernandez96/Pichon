f4247aabcb16c7549854e07312630a14d20d7d61
LuisHernandez96 <luis_fernando_hernandez@outlook.com>
2018-04-20 17:07:03 -0500

    VM Part 2: all operators

--- VMachine.py
+++ VMachine.py
@@ -1,7 +1,23 @@
 GOTO = "GOTO"
+GOTOF = "GOTO_FALSE"
+GOTOT = "GOTO_TRUE"
+VER = "VER"
 PLUS = "+"
 MINUS = "-"
-EQUAL = "="
+MULT = "*"
+DIV = "/"
+ASSIGN = "="
+OR = "||"
+AND = "&&"
+LESS_THAN = "<"
+GREAT_THAN = ">"
+EQL_LESS_THAN = "<="
+EQL_GREAT_THAN = ">="
+EQL = "=="
+DIF = "!="
+NOT = "!"
+NEG = "~"
+
 
 
 class VMachine:
@@ -44,7 +60,29 @@ class VMachine:
             else:
                 sys.exit("Error in save result MINUS")
 
-        elif cuadruplo.operator == EQUAL:
+        elif cuadruplo.operator == MULT:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            result = oper1 * oper2
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result MULT")
+
+        elif cuadruplo.operator == DIV:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            result = oper1 / oper2
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result DIV")
+
+        elif cuadruplo.operator == ASSIGN:
             oper1 = self._getValue(cuadruplo.operand1)
 
             if self._saveResult(oper1, cuadruplo.result):
@@ -52,11 +90,156 @@ class VMachine:
             else:
                 sys.exit("Error in save result EQUAL")
 
+        elif cuadruplo.operator == OR:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 or oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result OR")
+
+        elif cuadruplo.operator == AND:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 and oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result AND")
+
+        elif cuadruplo.operator == LESS_THAN:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 < oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result LESS_THAN")
+
+        elif cuadruplo.operator == GREAT_THAN:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 > oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result GREAT_THAN")
+
+        elif cuadruplo.operator == EQL_LESS_THAN:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 <= oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result EQL_LESS_THAN")
+
+        elif cuadruplo.operator == EQL_GREAT_THAN:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 >= oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result EQL_GREAT_THAN")
+
+        elif cuadruplo.operator == EQL:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 == oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result EQL")
+
+        elif cuadruplo.operator == DIF:
+            oper1 = self._getValue(cuadruplo.operand1)
+            oper2 = self._getValue(cuadruplo.operand2)
+
+            if oper1 != oper2:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result DIF")
+
+        elif cuadruplo.operator == NOT:
+            oper1 = self._getValue(cuadruplo.operand1)
+
+            if not oper1:
+                result = True
+            else:
+                result = False
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result NOT")
+
+        elif cuadruplo.operator == NEG:
+            oper1 = self._getValue(cuadruplo.operand1)
+
+            result = oper1 * -1
+
+            if self._saveResult(result, cuadruplo.result):
+                self.currentCuad += 1
+            else:
+                sys.exit("Error in save result NEG")
+
+        print("")
+
     def _getValue(self,operand):
         try:
             if operand[0] == "%":
                 operand = operand.replace("%","")
-                return int(operand)
+                try:
+                    return int(operand)
+                except:
+                    if operand == "true":
+                        return True
+                    elif operand == "false":
+                        return False
+                    else:
+                        return "KEK_ERROR"
 
             elif operand[0] == "(":
                 operand = operand.replace("(", "")
@@ -86,15 +269,15 @@ class VMachine:
             if self.INT_MEME[memDirection % 40000] == None:
                 pass
             else:
-                return self.INT_MEME[memDirection % 40000]
+                return int(self.INT_MEME[memDirection % 40000])
         elif memDirection > 59999:
             if self.BOOL_MEME[memDirection % 60000] == None:
                 pass
             else:
-                return self.INT_MEME[memDirection % 60000]
+                return self.BOOL_MEME[memDirection % 60000]
         else:
-            if self.BOOL_MEME[memDirection % 50000] == None:
+            if self.FLOAT_MEME[memDirection % 50000] == None:
                 pass
             else:
-                return self.INT_MEME[memDirection % 50000]
+                return self.FLOAT_MEME[memDirection % 50000]
 
